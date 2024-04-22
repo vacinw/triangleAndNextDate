0eb059274de7b5d2ff1bdaf54c16bcae263b097f
Tâm Nguyễn <tamnguyen@Tams-MacBook-Pro.local>
2024-04-22 17:36:47 +0700

    add test

---
+++ src/AppTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testNot(){
+        assertEquals("notATriangle", App.triangle(10, 2, 4));
+    }
+
+    @Test
+    public void testEquilateral(){
+        assertEquals("Equilateral", App.triangle(3, 3, 3));
+    }
+
+    @Test
+    public void testIsosceles(){
+        assertEquals("Isosceles", App.triangle(3, 3, 4));
+    }
+
+    @Test
+    public void testScalene(){
+        assertEquals("Scalene", App.triangle(2, 3, 4));
+    }
+}
