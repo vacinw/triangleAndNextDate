8ff23e1540971cee9fea1a0302899947bc2d6b9c
Tâm Nguyễn <tamnguyen@Tams-MacBook-Pro.local>
2024-04-22 17:12:27 +0700

    first cm

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+
+    public static void main(String[] args) {
+        int a = 2, b = 3, c = 4;
+
+        System.out.println("Loai tam giac: " + triangle(a, b, c));
+       
+    }
+    public static String triangle(int a, int b, int c){
+        String loaiTamGiac;
+        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || a + c <= b) {
+            loaiTamGiac = "notATriangle"; // Không phải tam giác
+        } else {
+    
+            if (a == b && b == c) {
+                loaiTamGiac = "Equilateral"; // Tam giác đều
+            } else if (a == b || b == c || a == c) {
+                loaiTamGiac = "Isosceles"; // Tam giác cân
+            } 
+            // else if (){
+            //     loaiTamGiac = "rightTriangle";
+            // }
+            else {
+                loaiTamGiac = "Scalene"; // Tam giác thường
+            }
+        }
+
+        // Hiển thị kết quả
+        return loaiTamGiac;
+    }
+}
